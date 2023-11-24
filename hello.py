5edbe4ab3ce1f416452acc6700cc00f0525d6dbe
chrismatchett <chrismatchett@hotmail.com>
2023-11-24 14:38:45 +0000

    simple hello world for app service

---
+++ hello.py
@@ -0,0 +1,19 @@
+# hello.py
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class SimpleHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(b"Hello, World!")
+
+def run_server(port=8000):
+    server_address = ('', port)
+    httpd = HTTPServer(server_address, SimpleHandler)
+    print(f"Server started on port {port}")
+    httpd.serve_forever()
+
+if __name__ == '__main__':
+    run_server()
